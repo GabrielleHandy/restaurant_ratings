de7ff357a9cb607a44ccf9f3298e7afc76eb2cb7
gabby <gehandy123@gmail.com>
2021-11-09 00:23:43 +0000

    Finished main excercise!

--- ratings.py
+++ ratings.py
@@ -1,4 +1,55 @@
 """Restaurant rating lister."""
 
 
+
 # put your code here
+def get_restaurant_dict(restaurant_file):
+    opened_file = open(restaurant_file)
+
+    restaurant_dict = {}
+
+
+    for line in opened_file:
+        line = line.rstrip()
+        rest_list = line.split(':')
+        
+        restaurant_dict[rest_list[0]] = int(rest_list[1])
+    
+    opened_file.close()
+    return restaurant_dict
+    
+
+def readable_list(dictionary):
+    sentences = []
+    for restaurant in dictionary.items():
+        sentences.append(f"{restaurant[0]} is rated at {restaurant[1]}")
+        
+    return sentences
+    #     print(f"{restaurant_dict[restaurant]}")
+    
+
+def input_restaurant(dict):
+
+    name = input("Enter restaurant name: ")
+    rating = int(input("Enter rating: "))
+
+    dict[name] = rating
+
+    return dict
+
+
+def print_rest_list(restaurant_list):
+
+    for sentence in restaurant_list:
+        print(sentence)
+
+
+rest_dict = get_restaurant_dict("scores.txt")
+
+read_list = readable_list(rest_dict)
+
+print_rest_list(read_list)
+
+read_list = readable_list(input_restaurant(rest_dict))
+
+print_rest_list(read_list)
